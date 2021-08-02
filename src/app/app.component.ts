24178f91c60b4f22e191163f689c8354b1684267
TheMaximeSan <mxm.tournier@gmail.com>
2021-08-02 16:29:27 +0200

    category all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   title = 'JeSuisUnTitre';
 
   chucks:any = [];
+  category:any = [];
 
   chucksTest = [
     {"categories":["TEST"],"created_at":"2020-01-05 13:42:24.696555","icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"lulpsYvTQROW5q3GNJRVXQ","updated_at":"2020-01-05 13:42:24.696555","url":"https://api.chucknorris.io/jokes/lulpsYvTQROW5q3GNJRVXQ","value":"Hookers have nicknamed Chuck Norris \"hemorrhoid\" because of the residual pain in thier assholes."},
@@ -18,7 +19,13 @@ export class AppComponent {
     {"categories":["TEST"],"created_at":"2020-01-05 13:42:30.730109","icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"H3dOPTjuS1y3v5Bf1-XQvQ","updated_at":"2020-05-22 06:16:41.133769","url":"https://api.chucknorris.io/jokes/H3dOPTjuS1y3v5Bf1-XQvQ","value":"Homeless people give money to Chuck Norris. The last time Chuck Norris went surfing off the coast of Florida he bit a shark. Chuck Norris doesn't have a problem with alcohol."}
   ]
 
-  constructor(private httpClient: HttpClient) { }
+
+
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.get('https://api.chucknorris.io/jokes/categories').subscribe(LesCategory => {
+      this.category = LesCategory;
+    });
+  }
 
   hideChucks(){
       this.chucks = [];

e2ef5fed9b301acfadbf0a8c3eb68c01eda91a3a
TheMaximeSan <mxm.tournier@gmail.com>
2021-08-02 14:54:04 +0200

    requete get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'JeSuisUnTitre';
 
-  chucks:any = null;
-  btnText= "Afficher Blague";
+  chucks:any = [];
+  chuckTest:any = [{
+    "categories":[]
+  }];
 
   chucksFutur = [
     {"categories":[],"created_at":"2020-01-05 13:42:24.696555","icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"lulpsYvTQROW5q3GNJRVXQ","updated_at":"2020-01-05 13:42:24.696555","url":"https://api.chucknorris.io/jokes/lulpsYvTQROW5q3GNJRVXQ","value":"Hookers have nicknamed Chuck Norris \"hemorrhoid\" because of the residual pain in thier assholes."},
@@ -17,15 +21,30 @@ export class AppComponent {
     {"categories":["money"],"created_at":"2020-01-05 13:42:30.730109","icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png","id":"H3dOPTjuS1y3v5Bf1-XQvQ","updated_at":"2020-05-22 06:16:41.133769","url":"https://api.chucknorris.io/jokes/H3dOPTjuS1y3v5Bf1-XQvQ","value":"Homeless people give money to Chuck Norris. The last time Chuck Norris went surfing off the coast of Florida he bit a shark. Chuck Norris doesn't have a problem with alcohol."}
   ]
 
-  showChucks(){
-    if (this.chucks == null) {
-      this.chucks = this.chucksFutur
-      this.btnText = "Cacher Blague";
-    }else{
-      this.chucks = null
-      this.btnText = "Afficher Blague";
-    }
+  hideChucks(){
+      this.chucks = [];
   }
+
+  getJokeFromApi(){
+    this.httpClient.get('https://api.chucknorris.io/jokes/random')
+      .subscribe(LeChuckDeReponse => {
+        this.chucks.push(LeChuckDeReponse);
+    });
+  }
+
+  getJokeMoneyFromApi(){
+    while (this.chuckTest.categories == "money") {
+      this.httpClient.get('https://api.chucknorris.io/jokes/random')
+        .subscribe(LeChuckDeReponse => {
+          this.chuckTest.push(LeChuckDeReponse);
+        });
+      console.log(this.chuckTest.categories);
+    }
+
+  }
+
+  constructor(private httpClient: HttpClient) { }
+
 }
 
 
